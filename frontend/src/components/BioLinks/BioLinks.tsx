518c85ee6eee1d5c63002e42116fd082c09b66f8
Mark <markfitz815@gmail.com>
2024-12-30 23:48:56 +0000

    Update resume icon to match sizing of other icons

--- frontend/src/components/BioLinks/BioLinks.tsx
+++ frontend/src/components/BioLinks/BioLinks.tsx
@@ -10,7 +10,7 @@ function BioLinks() {
         <a href='https://www.linkedin.com/in/mark-fitzpatrick-491419100/' target='_blank' rel='noopener noreferrer'><li title="LinkedIn"><FontAwesomeIcon icon={faLinkedin} /></li></a>
         <a href='https://github.com/Havolt' target='_blank' rel='noopener noreferrer'><li title="GitHub"><FontAwesomeIcon icon={faGithub} /></li></a>
         <a href='mailto:markfitz815@gmail.com' target='_blank' rel='noopener noreferrer'><li title="Email"><FontAwesomeIcon icon={faEnvelope} /></li></a>
-        <a href={cv} target='_blank' rel='noopener noreferrer'><li title="Resume"><FontAwesomeIcon icon={faFileLines} /></li></a>
+        <a href={cv} target='_blank' rel='noopener noreferrer' className='bio-links__resume'><li title="Resume"><FontAwesomeIcon icon={faFileLines} /></li></a>
     </ul>
   )
 }
