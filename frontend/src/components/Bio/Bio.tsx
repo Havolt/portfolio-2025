06b54df5c614c5546f9e7ea212298dda9474b37d
Mark <markfitz815@gmail.com>
2025-11-23 18:22:03 +0000

    Make transition more seamless

--- frontend/src/components/Bio/Bio.tsx
+++ frontend/src/components/Bio/Bio.tsx
@@ -6,18 +6,31 @@ import BioPortfolio from "@/components/BioPortfolio/BioPortfolio";
 import "./Bio.scss";
 
 export default function Bio() {
+  const [hideBlurb, setHideBlurb] = useState(false);
   const [expandPortfolio, setExpandPortfolio] = useState(false);
 
   const togglePortfolio = () => {
+    const swapTime = 400;
     console.log("Toggling portfolio");
-    setExpandPortfolio(!expandPortfolio);
+    if (!expandPortfolio) {
+      setHideBlurb(true);
+      setTimeout(() => {
+        setExpandPortfolio(!expandPortfolio);
+      }, swapTime);
+    } else {
+      setExpandPortfolio(!expandPortfolio);
+      setTimeout(() => {
+        setHideBlurb(false);
+      }, swapTime);
+    }
+    // setExpandPortfolio(!expandPortfolio);
   };
 
   return (
     <div className="bio">
       <BioImage expandPortfolio={expandPortfolio} />
       <h1 className="bio__title">Hi, I'm Mark</h1>
-      <BioBlurb className={`${expandPortfolio ? "hide" : ""}`} />
+      <BioBlurb className={`${hideBlurb ? "hide" : ""}`} />
       <BioPortfolio
         handleTogglePortfolio={togglePortfolio}
         expandPortfolio={expandPortfolio}
